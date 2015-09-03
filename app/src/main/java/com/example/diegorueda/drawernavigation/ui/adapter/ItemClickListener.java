f0119f49d94ca96b08a57bab1958760ed0379295
Diego Rueda <diego.rueda2@gmail.com>
2015-09-03 18:44:54 -0400

    interface

---
+++ app/src/main/java/com/example/diegorueda/drawernavigation/ui/adapter/ItemClickListener.java
@@ -0,0 +1,10 @@
+package com.example.diegorueda.drawernavigation.ui.adapter;
+
+import android.view.View;
+
+/**
+ * Created by diego.rueda on 9/3/2015.
+ */
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
